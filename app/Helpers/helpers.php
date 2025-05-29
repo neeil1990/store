c1e642c8c16d405b9b880dd6bfbe68b2c0cd3437
Neil <neeil@mail.ru>
2025-05-29 12:38:03 +0100

    shipper module v1.9 currency bugfix

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -3,13 +3,13 @@
 if (!function_exists('money')) {
     function money(float $amount): string
     {
-        return number_format($amount, 2, ',' , ' ');
+        return number_format($amount, 2, '.' , ' ');
     }
 }
 
 if (!function_exists('amount')) {
     function amount(int $amount): string
     {
-        return number_format($amount, 0, ',' , ' ');
+        return number_format($amount, 0, '.' , ' ');
     }
 }
