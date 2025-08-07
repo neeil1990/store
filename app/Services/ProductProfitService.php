3288fdb540b33641a2228df667a7065aecb08fac
neeil1990 <neeil@mail.ru>
2025-08-07 17:01:25 +0300

    Update ProductProfitService

--- app/Services/ProductProfitService.php
+++ app/Services/ProductProfitService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services;
 
+use App\Services\BundleService;
+use Illuminate\Support\Arr;
+
 class ProductProfitService
 {
-
     public function getProfitByProduct(string $uuid, string $date): array
     {
         $api = new \App\Lib\Moysklad\MojSkladJsonApi;
@@ -17,5 +19,26 @@ class ProductProfitService
         ]);
 
         return $api->getRows();
+    }
+
+    public function getTotalSell(string $uuid, string $dateFrom)
+    {
+        $selling = 0;
+
+        $bundles = (new BundleService())->getBundleByProduct($uuid);
+
+        $uuids = Arr::pluck($bundles, 'uuid');
+
+        $uuids[] = $uuid;
+
+        foreach ($uuids as $uuid) {
+            $profit = $this->getProfitByProduct($uuid, $dateFrom);
+
+            if ($profit) {
+                $selling += $profit[0]['sellQuantity'];
+            }
+        }
+
+        return $selling;
     }
 }
