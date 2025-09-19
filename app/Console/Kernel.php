da9ff43eca54c2a1fee3bb2120489cf1f98ecd5c
neeil1990 <neeil@mail.ru>
2025-09-19 16:43:38 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('app:calculate-shipper-fields')->hourly();
+        $schedule->command('app:calculate-shipper-fields')->everyMinute();
     }
 
     /**
